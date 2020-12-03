74e20a77e4c1feb000f6a7b578a7054b8516dd18
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-04 03:19:31 +0900

    add: api test

---
+++ module-react+jsp/src/main/java/com/api/ServletTest.java
@@ -0,0 +1,28 @@
+package com.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletTest", urlPatterns = "/api")
+public class ServletTest extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("utf-8");
+        String id = request.getParameter("id");
+        String pw = request.getParameter("pw");
+
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("id: " + id);
+        out.println("pw: " + pw);
+    }
+}
--- module-react+jsp/src/main/webapp/index.jsp
+++ module-react+jsp/src/main/webapp/index.jsp
@@ -1,9 +1,31 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Title</title>
-  </head>
-  <body>
-  Hello, world! 현재 페이지는 jsp입니다!
-  </body>
+    <style>
+        #root {
+            text-align: center;
+            background-color: #00D8FF;
+            border: 1px solid #5D5D5D;
+            position: absolute;
+            height: 300px;
+            width: 400px;
+            margin: -150px 0px 0px -200px;
+            top: 50%;
+            left: 50%;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<div id="root">
+    <span style="font-size: 24px; margin: 10px 10px 10px 10px">Hello, world! 현재 페이지는 jsp입니다!</span>
+    <form action="/api" method="get">
+        ID: <input type="text" name="id" value=""><br/>
+        PW: <input type="text" name="pw" value=""><br/>
+        <input type="submit" value="Submit">
+    </form>
+</div>
+</body>
 </html>
