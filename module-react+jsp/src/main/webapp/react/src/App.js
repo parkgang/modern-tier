e946eae692ce65591ec207ff52a13a3d8cedc747
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-05 14:34:52 +0900

    init: css, hotloader를 제외한 react project init

---
+++ module-react+jsp/src/main/webapp/react/src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>Hello, Webpack!</div>;
+
+export default App;
---
+++ module-react+jsp/src/main/webapp/react/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    open: true,
+  },
+};
