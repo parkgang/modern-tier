aa41dd9826383a7c64dd3305a99b940c1b84f796
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-12 19:24:15 +0900

    add: 1차 버전 sql

--- module-react+jsp/src/main/resources/SQL_Query/modern_tier.sql
+++ module-react+jsp/src/main/resources/SQL_Query/modern_tier.sql
@@ -4,10 +4,26 @@ create database modern_tier;
 
 use modern_tier;
 
+show tables;
+
 CREATE TABLE user
 (
-    id             INT(11)      NOT NULL AUTO_INCREMENT,
-    kakao_id       VARCHAR(100) NOT NULL,
-    kakao_nickname VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+    id                  INT(11) NOT NULL AUTO_INCREMENT,
+    kakao_id            INT     NOT NULL UNIQUE,
+    kakao_nickname      VARCHAR(20),
+    kakao_email         VARCHAR(20),
+    kakao_access_token  VARCHAR(60),
+    kakao_refresh_token VARCHAR(60),
+    riot_id             VARCHAR(60) UNIQUE,
+    riot_name           VARCHAR(20),
+    riot_summonerLevel  INT,
+    PRIMARY KEY (id, kakao_id)
 );
+
+drop table user;
+
+select *
+from user;
+
+insert into user
+values (null, 2, null, null, null, null, null, null, 1);
