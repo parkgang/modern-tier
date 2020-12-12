3a7b424848ef2aa5de6597627ba1c67e5856bf55
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-12 16:09:57 +0900

    refactor: kakao 토큰 받기 api 의존성 분리

--- module-react+jsp/src/main/java/com/api/oauth/Kakao.java
+++ module-react+jsp/src/main/java/com/api/oauth/Kakao.java
@@ -15,19 +15,18 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
 
 @Path("oauth/kakao")
 public class Kakao {
     private final String Kakao_Token_Req_URI = "https://kauth.kakao.com/oauth/token";
     private final String OAuth_Redirect_URI = "http://" + Service.LOCAL_IP + "/api/oauth/kakao";
 
-    @GET
-    public Response login(@QueryParam("code") String code) {
-        try {
-            // 인가 코드 확인
-            System.out.println("code: " + code);
+    public String getAccessToken(String authorize_code) {
+        String access_token = null;
+        String refresh_token = null;
 
-            // 토큰 받기
+        try {
             URL url = new URL(Kakao_Token_Req_URI);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -41,18 +40,66 @@ public class Kakao {
             sb.append("grant_type=" + "authorization_code");
             sb.append("&client_id=" + KakaoApp.REST_API_KEY);
             sb.append("&redirect_uri=" + OAuth_Redirect_URI);
-            sb.append("&code=" + code);
+            sb.append("&code=" + authorize_code);
             bw.write(sb.toString());
             bw.flush();
 
             // 결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
+            System.out.println("responseCode: " + responseCode);
             if (responseCode != 200) {
                 System.out.println("에러입니다. 로그를 확인해주세요.");
             }
 
             // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body: " + result);
+
+            // Response JSON 파싱
+            JSONObject jObject = new JSONObject(result);
+            access_token = jObject.getString("access_token");
+            refresh_token = jObject.getString("refresh_token");
+            System.out.println("access_token: " + access_token);
+            System.out.println("refresh_token: " + refresh_token);
+
+            br.close();
+            bw.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return access_token;
+    }
+
+    @GET
+    public Response login(@QueryParam("code") String code) {
+        try {
+            // 인가 코드 확인
+            System.out.println("code: " + code);
+
+            String access_token = getAccessToken(code);
+
+            /// 사용자 정보 가져오기 (테스트)
+            // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+            HashMap<String, Object> userInfo = new HashMap<>();
+            String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            // 요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer " + access_token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
             String line = "";
@@ -62,18 +109,7 @@ public class Kakao {
                 result += line;
             }
             System.out.println("response body : " + result);
-
-            // Response JSON 파싱
-            JSONObject jObject = new JSONObject(result);
-            String access_token = jObject.getString("access_token");
-            String refresh_token = jObject.getString("refresh_token");
-            System.out.println("access_token: " + access_token);
-            System.out.println("refresh_token: " + refresh_token);
-
-            /// 사용자 정보 가져오기 (테스트)
             ///
-
-
 
             // react page forward test
             // request.getRequestDispatcher("/react/dist/").forward(request, response);
