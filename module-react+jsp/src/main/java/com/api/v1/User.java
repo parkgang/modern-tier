5f8e2f9c592fb1de3271f866b185061044384fbc
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-13 06:19:33 +0900

    add: responce json

--- module-react+jsp/src/main/java/com/api/v1/User.java
+++ module-react+jsp/src/main/java/com/api/v1/User.java
@@ -1,19 +1,29 @@
 package com.api.v1;
 
+import org.json.JSONObject;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/v1/user")
 public class User {
 
     @GET
     @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
     // 로그인 확인
-    public String login(@Context HttpServletRequest req) {
+    public Response login(@Context HttpServletRequest req) {
         try {
-            return req.getSession().getAttribute("kakao_id").toString();
+            int kakao_id = (int) req.getSession().getAttribute("kakao_id");
+
+            JSONObject object = new JSONObject();
+            object.put("kakao_id", kakao_id);
+            return Response.status(Response.Status.OK).entity(object.toString()).build();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
