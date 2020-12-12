65edfd70d073d8b7e2ecdb63c3ca26545a901853
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-12 19:24:26 +0900

    add: local ip

--- module-react+jsp/src/main/java/com/constant/Service.java
+++ module-react+jsp/src/main/java/com/constant/Service.java
@@ -3,4 +3,5 @@ package com.constant;
 public final class Service {
     public static final String PROGRAM_VERSION = "v0.1";
     public static final String SERVER_IP = "52.231.50.84";
+    public static final String LOCAL_IP = "localhost:8080";
 }
