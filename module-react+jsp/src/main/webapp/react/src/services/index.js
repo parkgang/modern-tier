217572a3c3855afce519bc336eb3391c4cc798ce
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-14 04:25:14 +0900

    edit: server 배포 ip 변경

--- module-react+jsp/src/main/webapp/react/src/services/index.js
+++ module-react+jsp/src/main/webapp/react/src/services/index.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // eslint-disable-next-line no-unused-vars
 const SERVER_DOMAIN = 'http://52.231.50.84/';
+// eslint-disable-next-line no-unused-vars
 const LOCAL_DOMAIN = 'http://localhost:8080/';
-export const USE_DOMAIN = LOCAL_DOMAIN;
+export const USE_DOMAIN = SERVER_DOMAIN;
 
 export const getSession = () => {
   return axios.get(USE_DOMAIN + 'api/v1/user/login');
