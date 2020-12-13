b502a5d93392e0d059ca975723dff7d701a770d4
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-13 23:35:09 +0900

    edit: 무중단 배포 안정성 개선

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
 	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
-	sleep 10
+	sleep 20
 
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
 	echo "green up"
 	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
-	sleep 10
+	sleep 20
 
 	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 fi
