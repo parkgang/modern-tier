7d64b57b0aa958e4263f1da0a597a995f933f98d
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-15 08:08:32 +0900

    edit: 중요하지 않은 로그 주석처리

--- module-react+jsp/src/main/java/com/api/oauth/Kakao.java
+++ module-react+jsp/src/main/java/com/api/oauth/Kakao.java
@@ -86,7 +86,7 @@ public class Kakao {
 
             return access_token;
         } catch (Exception ex) {
-            System.out.println("getAccessToken 예러: " + ex);
+            System.out.println("getAccessToken 에러: " + ex);
         }
         return null;
     }
@@ -137,7 +137,7 @@ public class Kakao {
             userBean.setKakao_nickname(nickname);
             userBean.setKakao_email(email);
         } catch (Exception ex) {
-            System.out.println("getUserInfo 예러: " + ex);
+            System.out.println("getUserInfo 에러: " + ex);
         }
     }
 
@@ -170,7 +170,7 @@ public class Kakao {
             URI uri = new URI("/react/dist/");
             return Response.seeOther(uri).build();
         } catch (Exception ex) {
-            System.out.println("login 예러: " + ex);
+            System.out.println("login 에러: " + ex);
         }
         return null;
     }
