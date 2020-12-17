110c2acb41e70784a0ac0c200b1893693ff58b4a
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-18 01:28:51 +0900

    add: promise err catch 강화

--- module-react+jsp/src/main/webapp/react/src/hocs/withLogined.js
+++ module-react+jsp/src/main/webapp/react/src/hocs/withLogined.js
@@ -1,19 +1,31 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import * as actions from '../actions';
 import { Header, Profile, TabNavigator } from '../components';
-
-import { USE_DOMAIN, getSession } from '../services';
+import { getSession } from '../services';
+import { USE_DOMAIN } from '../constants';
 
 export default function (InputComponent) {
   function OAuthCheck(props) {
+    const dispatch = useDispatch();
     useEffect(() => {
-      getSession().then((res) => {
-        console.log(res.data.kakao_id);
-        const kakaoId = res.data.kakao_id;
-        if (kakaoId === undefined) {
-          window.location.href = USE_DOMAIN + 'views/kakaoOAuth/';
-        }
-      });
+      getSession()
+        .then((res) => {
+          const kakaoId = res.data.kakao_id;
+          console.log(`kakaoId: ${kakaoId}`);
+          if (kakaoId === undefined) {
+            window.location.href = USE_DOMAIN + '/views/kakaoOAuth/';
+          } else {
+            dispatch(actions.loginUser(kakaoId));
+          }
+        })
+        .catch((err) => {
+          console.error(
+            `REST API Server가 비활성화되어 있을 확률이 높습니다: ${err}`
+          );
+          window.location.href = USE_DOMAIN + '/views/kakaoOAuth/';
+        });
     }, []);
 
     return (
