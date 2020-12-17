c1dc1dbef9729fd09ce7d84afde33fa248522b75
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-18 00:52:36 +0900

    add: loginUser Action 함수

--- module-react+jsp/src/main/webapp/react/src/actions/ActionTypes.js
+++ module-react+jsp/src/main/webapp/react/src/actions/ActionTypes.js
@@ -1,5 +1,6 @@
 export const DOWN = 'DOWN';
 export const UP = 'UP';
 
+export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
 export const UNLINK_USER = 'UNLINK_USER';
--- module-react+jsp/src/main/webapp/react/src/actions/index.js
+++ module-react+jsp/src/main/webapp/react/src/actions/index.js
@@ -8,6 +8,17 @@ export const upCount = () => ({
   type: types.UP,
 });
 
+export const loginUser = (kakaoId) => {
+  console.log('loginUser:' + kakaoId);
+  // action에서 해당 id값에 해당하는 db 레코드 json으로 반환
+  // 반환된 json으로 카카오 api 호출
+  // 위의 모든 값은 리듀서로 전송
+  // return이 없으면 redux가 트리거 되지 않아서 정상적으로 렌더링 되지 않습니다.
+  return {
+    type: types.LOGIN_USER,
+  };
+};
+
 export const logoutUser = () => ({
   type: types.LOGOUT_USER,
 });
