8b65e563741fc5fe6fda2aa330e9ecf8fb248ae9
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-19 00:38:00 +0900

    add: 친구 추가, 로그아웃 등 팝업으로 처리

--- module-react+jsp/src/main/webapp/react/src/components/Header/index.js
+++ module-react+jsp/src/main/webapp/react/src/components/Header/index.js
@@ -1,16 +1,85 @@
 import React from 'react';
-import * as actions from '../../actions';
 import { useDispatch } from 'react-redux';
+
+import * as actions from '../../actions';
+import { PATH_ROOT } from '../../constants';
 
 import './index.css';
 
 const Header = () => {
   const dispatch = useDispatch();
 
+  // 레이어 팝업 설계의 문제로 생성된 함수 입니다. 다른 팝업이 활성화 되어있으면 종료합니다.
+  const popUpController = (mode) => {
+    let oppositePopUpId;
+    if (mode === 'burger-menu') {
+      oppositePopUpId = 'add-friend-toggle';
+    } else {
+      oppositePopUpId = 'burger-menu-toggle';
+    }
+    const toggle = document.getElementById(oppositePopUpId);
+    if (toggle.checked === true) {
+      toggle.click();
+    }
+  };
+
   return (
     <div id="header">
-      <button onClick={() => dispatch(actions.logoutUser())}>로그아웃</button>
-      <button onClick={() => dispatch(actions.unlinkUser())}>탈퇴</button>
+      <a href={PATH_ROOT}>
+        <img src="/src/resources/img/service-logo.jpg" />
+      </a>
+      <div className="burger-menu">
+        <input id="burger-menu-toggle" type="checkbox" />
+        <label
+          htmlFor="burger-menu-toggle"
+          onClick={() => {
+            popUpController('burger-menu');
+          }}
+        >
+          <img src="/src/resources/icon/burger-menu.svg" width="24px" />
+        </label>
+        <div id="burger-menu-list">
+          <ul>
+            <li>
+              <button onClick={() => dispatch(actions.logoutUser())}>
+                로그아웃
+              </button>
+            </li>
+            <li>
+              <button onClick={() => dispatch(actions.unlinkUser())}>
+                탈퇴
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="add-friend">
+        <input id="add-friend-toggle" type="checkbox" />
+        <label
+          htmlFor="add-friend-toggle"
+          onClick={() => {
+            popUpController('add-friend');
+          }}
+        >
+          <img src="/src/resources/icon/add-friend.svg" width="24px" />
+        </label>
+        <div id="friend-list">
+          <input type="text" placeholder="이름으로 검색" />
+          <ul>
+            <li>
+              <img
+                src="/src/resources/img/kakaoTalk-default-profile.jpg"
+                width="30px"
+                alt=""
+              />
+              <span>박경은</span>
+              <div>
+                <button>친구 추가</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
