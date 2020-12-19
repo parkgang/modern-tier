4fa6ca8bc4066fc102b48b088411cbd7c4a48958
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-20 02:53:09 +0900

    add: searchUser reducer

--- module-react+jsp/src/main/webapp/react/src/reducers/Friend.js
+++ module-react+jsp/src/main/webapp/react/src/reducers/Friend.js
@@ -4,7 +4,7 @@ const initialState = {
   keyword: '',
   list: [
     {
-      kakaoId: '',
+      kakaoId: Number(null),
       nickname: '',
       profileImage: '',
       isFriend: false,
@@ -15,21 +15,21 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.SEARCH_USER:
-      return { ...state, keyword: action.payload };
+      return {
+        ...state,
+        keyword: action.nickname,
+        list: action.payload.map((x) => ({
+          kakaoId: x.kakaoId,
+          nickname: x.nickname,
+          profileImage:
+            x.profileImage === undefined
+              ? '/react/src/resources/img/kakaoTalk-default-profile.jpg'
+              : x.profileImage,
+          isFriend: x.isFriend,
+        })),
+      };
+    // return { ...state, keyword: action.nickname, list: action.payload };
     default:
       return state;
   }
 };
-
-/*
-const temp = {
-    kakaoId: '1',
-    nickname: '2',
-    profileImage: '3',
-    isFriend: false,
-  };
-  return {
-        ...state,
-        friend: [...state.friend, temp],
-      };
-*/
