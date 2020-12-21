1ec98b4bf2e63348f83b198c3151d648f1066bc6
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-21 18:44:56 +0900

    init: friend query

---
+++ module-react+jsp/src/main/resources/SQL_Query/friend.sql
@@ -0,0 +1,24 @@
+use modern_tier;
+
+# 조회
+select *
+from friend;
+
+# 삽입
+insert into friend (kakao_id, friend_kakao_id)
+values (1536694714, 1537406659);
+insert into friend (kakao_id, friend_kakao_id)
+values (1536694714, 1561304829);
+insert into friend (kakao_id, friend_kakao_id)
+values (1536694714, 1561343937);
+
+insert into friend (kakao_id, friend_kakao_id)
+values (1537406659, 1536897482);
+insert into friend (kakao_id, friend_kakao_id)
+values (1537406659, 1561304829);
+
+# 친구 조회
+select u.*
+from friend
+         inner join user u on friend.friend_kakao_id = u.kakao_id
+where friend.kakao_id = 1536694714;
