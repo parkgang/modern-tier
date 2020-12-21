a982bd4eab0be3df45d1537668b3a6c79a83c69d
박경은 <63892989+ruddms936@users.noreply.github.com>
2020-12-22 02:54:07 +0900

    edit: 탭 네비게이션 Ui 수정

--- module-react+jsp/src/main/webapp/react/src/components/TabNavigator/index.js
+++ module-react+jsp/src/main/webapp/react/src/components/TabNavigator/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { PATH_ROOT, PATH_COMPETITION } from '../../constants';
 
 import './index.css';
 
-const TabNavigator = () => {
+const TabNavigator = ({ page }) => {
   return (
     <div id="tabNavigator">
-      <Link to={PATH_ROOT}>
-        <button>랭킹</button>
-      </Link>
-      <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-      <Link to={PATH_COMPETITION}>
-        <button>경쟁</button>
-      </Link>
+      <ul>
+        <Link to={PATH_ROOT}>
+          <li className={page === 'RankingList' ? 'tab-select' : ''}>
+            <span>랭킹</span>
+          </li>
+        </Link>
+        <Link to={PATH_COMPETITION}>
+          <li className={page === 'CompetitionList' ? 'tab-select' : ''}>
+            <span>경쟁</span>
+          </li>
+        </Link>
+      </ul>
     </div>
   );
+};
+
+TabNavigator.propTypes = {
+  page: PropTypes.string,
 };
 
 export default TabNavigator;
